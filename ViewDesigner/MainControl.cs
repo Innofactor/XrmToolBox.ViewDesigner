05a39c7eb152cca4774797aa13b3a2771573c843
Alexey Shytikov <alexey.shytikov@gmail.com>
2016-08-05 11:14:32 +0200

    #1 Adding function for updating preview

--- ViewDesigner/MainControl.cs
+++ ViewDesigner/MainControl.cs
@@ -283,6 +283,25 @@
 
         private void tsbLivePreview_Click(object sender, EventArgs e)
         {
+            if (Service == null)
+            {
+                MessageBox.Show("Please connect to CRM.", ((ToolStripButton)sender).Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+            var view = ViewEditor != null ? ViewEditor.ToEntity() : null;
+            if (view == null || string.IsNullOrEmpty(view.LogicalName))
+            {
+                MessageBox.Show("First select a view to preview.", ((ToolStripButton)sender).Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            UpdatePreview();
+        }
+
+        private void UpdatePreview()
+        {
+
         }
     }
 }
